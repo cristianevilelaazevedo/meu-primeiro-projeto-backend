60e717240e7275a37cf22b0ee5d127545a5054f4
cristianevilelaazevedo <chrystianevilela27@gmail.com>
2023-04-30 21:32:36 -0300

    insere a nova versão do projeto integrado com o mongodb

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require("express")
-
+const { v4: uuidv4 } = require('uuid')
+const router = express.Router()
 const app = express()
 const porta = 3333
+
+//POST
+function criaMulher(request, response) {
+    const novaMulher = {
+        id: uuidv4(),
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio
+    }
+
+    mulheres.push(novaMulher)
+
+    response.json(mulheres)
+}
 
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta ", porta)
